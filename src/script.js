a782f6d83b8bf414f93bcf93e71cb2e1585190df
Fernandounuts <fernando.balieiro0580@hotmail.com>
2023-05-25 18:08:10 -0300

    guess my number

---
+++ src/script.js
@@ -0,0 +1,16 @@
+"using strict";
+/*
+document.querySelector(".message").textContent = "Correct number 🎉";
+document.querySelector(".number").textContent = 13;
+document.querySelector(".score").textContent = 15;
+// document.querySelector(".highscore").textContent = 17;
+// console.log(document.querySelector(".check").value);
+
+document.querySelector(".guess").value = 23;
+console.log(document.querySelector(".guess").value = 23);
+
+*/
+
+document.querySelector(".check").addEventListener("click", function () {
+  console.log(document.querySelector(".guess").value);
+});
